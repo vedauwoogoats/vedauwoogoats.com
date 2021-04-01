3152594c80e70ffbe0d2ccf4b9742733db21776a
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-04-01 12:33:54 -0600

    Create Doe model

--- app/resources/does/Does.jl
+++ app/resources/does/Does.jl
@@ -2,11 +2,24 @@ module Does
 
 import SearchLight: AbstractModel, DbId
 import Base: @kwdef
+using Dates
 
 export Doe
 
 @kwdef mutable struct Doe <: AbstractModel
   id::DbId = DbId()
+  RegistrationNumber::String = ""
+  FullName::String = ""
+  BirthDate::Date = Date(2000,1,1)
+  Breed::String = ""
+  Description::String = ""
+  Appraisals::String = ""
+  Placings::String = ""
+  MateNumber::String = ""
+  MateName::String = ""
+  BreedingDate::Date = Date(2000,1,1)
+  KiddingDate::Date = Date(2000,1,1)
+  SalePrice::Int = 0
 end
 
 end
