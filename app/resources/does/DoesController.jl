533bb5d2adae001ef06a24f7404501edbd9df1ef
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-04-01 18:25:20 -0600

    Add pages and routes for Does

--- app/resources/does/DoesController.jl
+++ app/resources/does/DoesController.jl
@@ -1,3 +1,9 @@
 module DoesController
+
+using Genie.Renderer.Html, SearchLight, Does
   # Build something great
+
+  function index()
+    html(:does, :index, does = all(Doe))
+  end
 end
--- routes.jl
+++ routes.jl
@@ -1,5 +1,8 @@
 using Genie.Router
+using DoesController
 
 route("/") do
   serve_static_file("welcome.html")
 end
+
+route("/does", DoesController.index)
